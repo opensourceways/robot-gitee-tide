6edbc9ea6c6fdba243ab9c55901691f4e72ef866
wanghaosq <shalldows@163.com>
2021-12-07 17:01:40 +0800

    add new function codes

--- config.go
+++ config.go
@@ -1,6 +1,17 @@
 package main
 
-import libconfig "github.com/opensourceways/community-robot-lib/config"
+import (
+	"fmt"
+	"regexp"
+
+	libconfig "github.com/opensourceways/community-robot-lib/config"
+)
+
+var (
+	checkPRRe          = regexp.MustCompile(`(?mi)^/check-pr\s*$`)
+	tideNotification   = "@%s, This pr is not mergeable."
+	tideNotificationRe = regexp.MustCompile(fmt.Sprintf(tideNotification, "(.*)"))
+)
 
 type configuration struct {
 	ConfigItems []botConfig `json:"config_items,omitempty"`
@@ -50,11 +61,79 @@ func (c *configuration) SetDefault() {
 
 type botConfig struct {
 	libconfig.PluginForRepo
+	Labels        []labelConfig        `json:"labels" required:"true"`
+	MissingLabels []missingLabelConfig `json:"missing_labels,omitempty"`
 }
 
 func (c *botConfig) setDefault() {
 }
 
 func (c *botConfig) validate() error {
-	return c.PluginForRepo.Validate()
+	if err := c.PluginForRepo.Validate(); err != nil {
+		return err
+	}
+
+	if len(c.Labels) == 0 {
+		return fmt.Errorf("missing required labels")
+	}
+
+	for _, v := range c.Labels {
+		if err := v.validate(); err != nil {
+			return err
+		}
+	}
+
+	for _, v := range c.MissingLabels {
+		if err := v.validate(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type labelConfig struct {
+	Label               string `json:"label" required:"true"`
+	TipsIfMissing       string `json:"tips_if_missing" required:"true"`
+	Person              string `json:"person,omitempty"`
+	TipsIfAddedByOthers string `json:"tips_if_added_by_others,omitempty"`
+}
+
+type missingLabelConfig struct {
+	Label          string `json:"label" required:"true"`
+	TipsIfExisting string `json:"tips_if_existing" required:"true"`
+}
+
+func (l *labelConfig) validate() error {
+	if l.Label == "" {
+		return fmt.Errorf("miss label")
+	}
+
+	if l.TipsIfMissing == "" {
+		return fmt.Errorf("miss TipsIfMissing")
+	}
+
+	if l.Person != "" && l.TipsIfAddedByOthers == "" {
+		return fmt.Errorf("must set tips_if_added_by_others if person is set")
+	}
+
+	return nil
+}
+
+func (m *missingLabelConfig) validate() error {
+	if m.Label == "" {
+		return fmt.Errorf("miss label")
+	}
+
+	if m.TipsIfExisting == "" {
+		return fmt.Errorf("missing tips_if_existing")
+	}
+
+	return nil
+}
+
+func (l *labelConfig) isAddByOthers(other string) (bool, string) {
+	b := l.Person != "" && l.Person != other
+
+	return b, l.TipsIfAddedByOthers
 }
--- robot.go
+++ robot.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+
+	"github.com/opensourceways/community-robot-lib/giteeclient"
+	"github.com/sirupsen/logrus"
 
 	sdk "gitee.com/openeuler/go-gitee/gitee"
 	libconfig "github.com/opensourceways/community-robot-lib/config"
 	libplugin "github.com/opensourceways/community-robot-lib/giteeplugin"
-	"github.com/sirupsen/logrus"
 )
 
-// TODO: set botName
-const botName = ""
+const botName = "tide"
 
 type iClient interface {
+	CreatePRComment(owner, repo string, number int32, comment string) error
+	DeletePRComment(org, repo string, ID int32) error
+	ListPRComments(org, repo string, number int32) ([]sdk.PullRequestComments, error)
+	MergePR(owner, repo string, number int32, opt sdk.PullRequestMergePutParam) error
+	ListPROperationLogs(org, repo string, number int32) ([]sdk.OperateLog, error)
 }
 
 func newRobot(cli iClient) *robot {
@@ -27,36 +33,122 @@ func (bot *robot) NewPluginConfig() libconfig.PluginConfig {
 	return &configuration{}
 }
 
-func (bot *robot) getConfig(cfg libconfig.PluginConfig) (*configuration, error) {
-	if c, ok := cfg.(*configuration); ok {
-		return c, nil
+func (bot *robot) getConfig(cfg libconfig.PluginConfig, org, repo string) (*botConfig, error) {
+	c, ok := cfg.(*configuration)
+	if !ok {
+		return nil, fmt.Errorf("can't convert to configuration")
 	}
-	return nil, errors.New("can't convert to configuration")
+
+	if bc := c.configFor(org, repo); bc != nil {
+		return bc, nil
+	}
+
+	return nil, fmt.Errorf("no config for this repo:%s/%s", org, repo)
 }
 
 func (bot *robot) RegisterEventHandler(p libplugin.HandlerRegitster) {
-	p.RegisterIssueHandler(bot.handleIssueEvent)
 	p.RegisterPullRequestHandler(bot.handlePREvent)
 	p.RegisterNoteEventHandler(bot.handleNoteEvent)
-	p.RegisterPushEventHandler(bot.handlePushEvent)
 }
 
-func (bot *robot) handlePREvent(e *sdk.PullRequestEvent, cfg libconfig.PluginConfig, log *logrus.Entry) error {
-	// TODO: if it doesn't needd to hand PR event, delete this function.
+func (bot *robot) handlePREvent(e *sdk.PullRequestEvent, pc libconfig.PluginConfig, log *logrus.Entry) error {
+	prInfo := giteeclient.GetPRInfoByPREvent(e)
+
+	if e.GetAction() != "open" {
+		log.Debug("Pull request state is not open, skipping...")
+
+		return nil
+	}
+
+	if e.GetActionDesc() != giteeclient.PRActionUpdatedLabel {
+		return nil
+	}
+
+	cfg, err := bot.getConfig(pc, prInfo.Org, prInfo.Repo)
+	if err != nil {
+		return err
+	}
+
+	return bot.handleMerge(prInfo, checkAllLabelsAreReady, cfg)
+}
+
+func (bot *robot) handleNoteEvent(e *sdk.NoteEvent, pc libconfig.PluginConfig, log *logrus.Entry) error {
+	ne := giteeclient.NewPRNoteEvent(e)
+	prInfo := ne.GetPRInfo()
+
+	if !ne.IsCreatingCommentEvent() {
+		log.Debug("Event is not a creation of a comment, skipping.")
+
+		return nil
+	}
+
+	if !ne.IsPullRequest() || !checkPRRe.MatchString(ne.GetComment()) {
+		return nil
+	}
+
+	if !ne.PullRequest.Mergeable {
+
+		return bot.writePRComment(prInfo, "Because it conflicts to the target branch.")
+	}
+
+	cfg, err := bot.getConfig(pc, prInfo.Org, prInfo.Repo)
+	if err != nil {
+		return err
+	}
+
+	return bot.handleMerge(prInfo, nil, cfg)
+}
+
+func (bot *robot) writePRComment(pr giteeclient.PRInfo, comment string) error {
+	org := pr.Org
+	repo := pr.Repo
+	number := pr.Number
+	author := pr.Author
+	err := bot.deleteOldComments(org, repo, number)
+
+	if err != nil {
+		return err
+	}
+
+	return bot.cli.CreatePRComment(org, repo, number, fmt.Sprintf(tideNotification, author)+comment)
+}
+
+func (bot *robot) deleteOldComments(org, repo string, prNumber int32) error {
+	comments, err := bot.cli.ListPRComments(org, repo, prNumber)
+	if err != nil {
+
+		return err
+	}
+
+	for _, c := range comments {
+
+		if c.User == nil {
+			continue
+		}
+
+		if tideNotificationRe.MatchString(c.Body) {
+			bot.cli.DeletePRComment(org, repo, c.Id)
+		}
+	}
+
 	return nil
 }
 
-func (bot *robot) handleIssueEvent(e *sdk.IssueEvent, cfg libconfig.PluginConfig, log *logrus.Entry) error {
-	// TODO: if it doesn't needd to hand Issue event, delete this function.
-	return nil
-}
+func (bot *robot) handleMerge(pr giteeclient.PRInfo, preCheck func(giteeclient.PRInfo, *botConfig) bool, cfg *botConfig) error {
+	if preCheck != nil && !preCheck(pr, cfg) {
 
-func (bot *robot) handlePushEvent(e *sdk.PushEvent, cfg libconfig.PluginConfig, log *logrus.Entry) error {
-	// TODO: if it doesn't needd to hand Push event, delete this function.
-	return nil
-}
+		return nil
+	}
 
-func (bot *robot) handleNoteEvent(e *sdk.NoteEvent, cfg libconfig.PluginConfig, log *logrus.Entry) error {
-	// TODO: if it doesn't needd to hand Note event, delete this function.
-	return nil
+	ops, err := bot.cli.ListPROperationLogs(pr.Org, pr.Repo, pr.Number)
+	if err != nil {
+		return err
+	}
+
+	if noteString := checkPRLabels(pr, cfg, ops); noteString != "" {
+
+		return bot.writePRComment(pr, "\n\n"+noteString)
+	}
+
+	return bot.cli.MergePR(pr.Org, pr.Repo, pr.Number, sdk.PullRequestMergePutParam{})
 }
